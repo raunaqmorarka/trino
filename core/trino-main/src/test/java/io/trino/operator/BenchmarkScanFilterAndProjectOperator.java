6e015dba0d5f917106940f3b737e76c9eaed3fb6
Raunaq Morarka <raunaqmorarka@gmail.com>
2025-04-21 18:54:17 +0530

    Benchmark RecordCursor vs PageSource for selective filter

--- core/trino-main/src/test/java/io/trino/operator/BenchmarkScanFilterAndProjectOperator.java
+++ core/trino-main/src/test/java/io/trino/operator/BenchmarkScanFilterAndProjectOperator.java
@@ -15,7 +15,7 @@ package io.trino.operator;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import io.airlift.slice.Slices;
+import io.airlift.slice.Slice;
 import io.airlift.units.DataSize;
 import io.trino.SequencePageBuilder;
 import io.trino.Session;
@@ -29,6 +29,8 @@ import io.trino.spi.Page;
 import io.trino.spi.connector.ColumnHandle;
 import io.trino.spi.connector.DynamicFilter;
 import io.trino.spi.connector.FixedPageSource;
+import io.trino.spi.connector.RecordCursor;
+import io.trino.spi.connector.RecordPageSource;
 import io.trino.spi.function.OperatorType;
 import io.trino.spi.type.Type;
 import io.trino.sql.PlannerContext;
@@ -37,25 +39,24 @@ import io.trino.sql.gen.ExpressionCompiler;
 import io.trino.sql.gen.PageFunctionCompiler;
 import io.trino.sql.gen.columnar.ColumnarFilterCompiler;
 import io.trino.sql.ir.Call;
-import io.trino.sql.ir.Cast;
 import io.trino.sql.ir.Comparison;
 import io.trino.sql.ir.Constant;
 import io.trino.sql.ir.Expression;
 import io.trino.sql.ir.Reference;
 import io.trino.sql.planner.Symbol;
 import io.trino.sql.planner.plan.PlanNodeId;
+import io.trino.sql.relational.InputReferenceExpression;
 import io.trino.sql.relational.RowExpression;
 import io.trino.sql.relational.SqlToRowExpressionTranslator;
 import io.trino.testing.TestingMetadata.TestingColumnHandle;
 import io.trino.testing.TestingSession;
 import io.trino.testing.TestingTaskContext;
 import io.trino.transaction.TestingTransactionManager;
+import io.trino.type.LikePattern;
 import org.junit.jupiter.api.Test;
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
@@ -69,11 +70,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static io.airlift.concurrent.Threads.daemonThreadsNamed;
 import static io.airlift.units.DataSize.Unit.GIGABYTE;
@@ -85,23 +89,28 @@ import static io.trino.spi.type.VarcharType.VARCHAR;
 import static io.trino.sql.analyzer.TypeSignatureProvider.fromTypes;
 import static io.trino.sql.ir.Comparison.Operator.EQUAL;
 import static io.trino.sql.planner.TestingPlannerContext.plannerContextBuilder;
+import static io.trino.sql.relational.Expressions.call;
+import static io.trino.sql.relational.Expressions.constant;
+import static io.trino.sql.relational.Expressions.field;
 import static io.trino.testing.TestingHandles.TEST_CATALOG_HANDLE;
 import static io.trino.testing.TestingHandles.TEST_TABLE_HANDLE;
 import static io.trino.testing.TestingSplit.createLocalSplit;
+import static io.trino.type.LikePatternType.LIKE_PATTERN;
 import static java.util.Locale.ENGLISH;
+import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.Executors.newCachedThreadPool;
 import static java.util.concurrent.Executors.newScheduledThreadPool;
 import static java.util.stream.Collectors.toList;
 import static org.openjdk.jmh.annotations.Scope.Thread;
 
 @State(Scope.Thread)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Fork(5)
-@Warmup(iterations = 10)
-@Measurement(iterations = 10)
-@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Fork(2)
+@Warmup(iterations = 10, time = 2)
+@Measurement(iterations = 10, time = 2)
 public class BenchmarkScanFilterAndProjectOperator
 {
+    private static final TestingFunctionResolution FUNCTION_RESOLUTION = new TestingFunctionResolution();
     private static final TestingTransactionManager TRANSACTION_MANAGER = new TestingTransactionManager();
     private static final PlannerContext PLANNER_CONTEXT = plannerContextBuilder()
             .withTransactionManager(TRANSACTION_MANAGER)
@@ -116,10 +125,7 @@ public class BenchmarkScanFilterAndProjectOperator
     private static final int FILTER_AND_PROJECT_MIN_OUTPUT_PAGE_ROW_COUNT = 256;
 
     private static final TestingFunctionResolution FUNCTIONS = new TestingFunctionResolution();
-    private static final ResolvedFunction CONCAT = FUNCTIONS.resolveFunction("concat", fromTypes(VARCHAR, VARCHAR));
-    private static final ResolvedFunction MODULUS_INTEGER = FUNCTIONS.resolveOperator(OperatorType.MODULUS, ImmutableList.of(INTEGER, INTEGER));
     private static final ResolvedFunction MODULUS_BIGINT = FUNCTIONS.resolveOperator(OperatorType.MODULUS, ImmutableList.of(BIGINT, BIGINT));
-    private static final ResolvedFunction ADD_BIGINT = FUNCTIONS.resolveOperator(OperatorType.ADD, ImmutableList.of(BIGINT, BIGINT));
 
     @State(Thread)
     public static class Context
@@ -130,17 +136,20 @@ public class BenchmarkScanFilterAndProjectOperator
         private ScheduledExecutorService scheduledExecutor;
         private OperatorFactory operatorFactory;
 
-        @Param({"32", "1024"})
-        int positionsPerPage = 32;
+        @Param({"1024"})
+        int positionsPerPage = 1024;
 
-        @Param({"2", "4", "8", "16", "32"})
-        int columnCount = 2;
+        @Param({"8"})
+        int columnCount = 8;
 
-        @Param({"varchar", "bigint"})
+        @Param({"varchar"})
         String type = "varchar";
 
-        @Param({"false", "true"})
-        boolean dictionaryBlocks;
+        @Param({"true", "false"})
+        boolean useRecordCursor;
+
+        @Param({"true", "false"})
+        boolean columnarFilterEvaluationEnabled;
 
         @Setup
         public void setup()
@@ -164,7 +173,8 @@ public class BenchmarkScanFilterAndProjectOperator
             CursorProcessorCompiler cursorProcessorCompiler = new CursorProcessorCompiler(PLANNER_CONTEXT.getFunctionManager());
             PageFunctionCompiler pageFunctionCompiler = new PageFunctionCompiler(PLANNER_CONTEXT.getFunctionManager(), 0);
             ColumnarFilterCompiler compiler = new ColumnarFilterCompiler(PLANNER_CONTEXT.getFunctionManager(), 0);
-            PageProcessor pageProcessor = new ExpressionCompiler(cursorProcessorCompiler, pageFunctionCompiler, compiler).compilePageProcessor(Optional.of(getFilter(type)), projections).get();
+            PageProcessor pageProcessor = new ExpressionCompiler(cursorProcessorCompiler, pageFunctionCompiler, compiler)
+                    .compilePageProcessor(columnarFilterEvaluationEnabled, Optional.of(getFilter(type)), Optional.empty(), projections, Optional.empty(), OptionalInt.empty()).apply(DynamicFilter.EMPTY);
             CursorProcessor cursorProcessor = new ExpressionCompiler(cursorProcessorCompiler, pageFunctionCompiler, compiler).compileCursorProcessor(Optional.of(getFilter(type)), projections, "key").get();
 
             createTaskContext();
@@ -184,7 +194,8 @@ public class BenchmarkScanFilterAndProjectOperator
                     0,
                     new PlanNodeId("test"),
                     new PlanNodeId("test_source"),
-                    (catalog) -> (session, split, table, columns, dynamicFilter) -> new FixedPageSource(inputPages),
+                    (catalog) -> (session, split, table, columns, dynamicFilter) ->
+                            useRecordCursor ? new RecordPageSource(types, new PageRecordCursor(types, inputPages)) : new FixedPageSource(inputPages),
                     () -> cursorProcessor,
                     (_) -> pageProcessor,
                     TEST_TABLE_HANDLE,
@@ -209,7 +220,7 @@ public class BenchmarkScanFilterAndProjectOperator
         {
             ImmutableList.Builder<Page> inputPagesBuilder = ImmutableList.builder();
             for (int i = 0; i < TOTAL_POSITIONS / positionsPerPage; ++i) {
-                inputPagesBuilder.add(createPage(types, positionsPerPage, dictionaryBlocks));
+                inputPagesBuilder.add(createPage(types, positionsPerPage));
             }
             return inputPagesBuilder.build();
         }
@@ -217,7 +228,10 @@ public class BenchmarkScanFilterAndProjectOperator
         private RowExpression getFilter(Type type)
         {
             if (type == VARCHAR) {
-                return rowExpression(new Comparison(EQUAL, new Call(MODULUS_INTEGER, ImmutableList.of(new Cast(new Reference(VARCHAR, "varchar0"), INTEGER), new Constant(INTEGER, 2L))), new Constant(INTEGER, 0L)));
+                return call(
+                        FUNCTION_RESOLUTION.resolveFunction("$like", fromTypes(VARCHAR, LIKE_PATTERN)),
+                        field(0, VARCHAR),
+                        constant(LikePattern.compile("%23", Optional.empty()), LIKE_PATTERN));
             }
             if (type == BIGINT) {
                 return rowExpression(new Comparison(EQUAL, new Call(MODULUS_BIGINT, ImmutableList.of(new Reference(INTEGER, "bigint0"), new Constant(INTEGER, 2L))), new Constant(INTEGER, 0L)));
@@ -228,17 +242,8 @@ public class BenchmarkScanFilterAndProjectOperator
         private List<RowExpression> getProjections(Type type)
         {
             ImmutableList.Builder<RowExpression> builder = ImmutableList.builder();
-            if (type == BIGINT) {
-                for (int i = 0; i < columnCount; i++) {
-                    builder.add(rowExpression(new Call(ADD_BIGINT, ImmutableList.of(new Reference(BIGINT, "bigint" + i), new Constant(BIGINT, 5L)))));
-                }
-            }
-            else if (type == VARCHAR) {
-                for (int i = 0; i < columnCount; i++) {
-                    // alternatively use identity expression rowExpression("varchar" + i, type) or
-                    // rowExpression("substr(varchar" + i + ", 1, 1)", type)
-                    builder.add(rowExpression(new Call(CONCAT, ImmutableList.of(new Reference(VARCHAR, "varchar" + i), new Constant(VARCHAR, Slices.utf8Slice("foo"))))));
-                }
+            for (int i = 0; i < columnCount; i++) {
+                builder.add(new InputReferenceExpression(i, type));
             }
             return builder.build();
         }
@@ -252,33 +257,30 @@ public class BenchmarkScanFilterAndProjectOperator
                     PLANNER_CONTEXT.getTypeManager());
         }
 
-        private static Page createPage(List<? extends Type> types, int positions, boolean dictionary)
+        private static Page createPage(List<? extends Type> types, int positions)
         {
-            if (dictionary) {
-                return SequencePageBuilder.createSequencePageWithDictionaryBlocks(types, positions);
-            }
             return SequencePageBuilder.createSequencePage(types, positions);
         }
     }
 
     @Benchmark
-    public List<Page> benchmarkColumnOriented(Context context)
+    public double benchmarkColumnOriented(Context context)
     {
         DriverContext driverContext = context.createTaskContext().addPipelineContext(0, true, true, false).addDriverContext();
         SourceOperator operator = (SourceOperator) context.getOperatorFactory().createOperator(driverContext);
 
-        ImmutableList.Builder<Page> outputPages = ImmutableList.builder();
+        long outputRows = 0;
         operator.addSplit(new Split(TEST_CATALOG_HANDLE, createLocalSplit()));
         operator.noMoreSplits();
 
-        for (int loops = 0; !operator.isFinished() && loops < 1_000_000; loops++) {
+        while (!operator.isFinished()) {
             Page outputPage = operator.getOutput();
             if (outputPage != null) {
-                outputPages.add(outputPage);
+                outputRows += outputPage.getPositionCount();
             }
         }
 
-        return outputPages.build();
+        return outputRows * 100.0 / TOTAL_POSITIONS;
     }
 
     @Test
@@ -286,7 +288,7 @@ public class BenchmarkScanFilterAndProjectOperator
     {
         Context context = new Context();
         context.setup();
-        benchmarkColumnOriented(context);
+        System.out.println(benchmarkColumnOriented(context));
         context.cleanup();
     }
 
@@ -294,5 +296,106 @@ public class BenchmarkScanFilterAndProjectOperator
             throws RunnerException
     {
         benchmark(BenchmarkScanFilterAndProjectOperator.class).run();
+    }
+
+    public static class PageRecordCursor
+            implements RecordCursor
+    {
+        private final List<Type> types;
+        private final List<Page> pages;
+        private int currentPageIndex;
+        private int position = -1;
+
+        private PageRecordCursor(List<Type> types, List<Page> pages)
+        {
+            this.types = ImmutableList.copyOf(requireNonNull(types, "types is null"));
+            this.pages = requireNonNull(pages, "page is null");
+            checkArgument(types.size() == pages.get(currentPageIndex).getChannelCount(), "Types do not match page channels");
+        }
+
+        @Override
+        public long getCompletedBytes()
+        {
+            return 0;
+        }
+
+        @Override
+        public long getReadTimeNanos()
+        {
+            return 0;
+        }
+
+        @Override
+        public Type getType(int field)
+        {
+            return types.get(field);
+        }
+
+        @Override
+        public boolean advanceNextPosition()
+        {
+            position++;
+            if (position >= pages.get(currentPageIndex).getPositionCount()) {
+                currentPageIndex++;
+                position = 0;
+            }
+            return currentPageIndex < pages.size();
+        }
+
+        @Override
+        public boolean getBoolean(int field)
+        {
+            checkState(position >= 0, "Not yet advanced");
+            checkState(position < pages.get(currentPageIndex).getPositionCount(), "Already finished");
+            Type type = types.get(field);
+            return type.getBoolean(pages.get(currentPageIndex).getBlock(field), position);
+        }
+
+        @Override
+        public long getLong(int field)
+        {
+            checkState(position >= 0, "Not yet advanced");
+            checkState(position < pages.get(currentPageIndex).getPositionCount(), "Already finished");
+            Type type = types.get(field);
+            return type.getLong(pages.get(currentPageIndex).getBlock(field), position);
+        }
+
+        @Override
+        public double getDouble(int field)
+        {
+            checkState(position >= 0, "Not yet advanced");
+            checkState(position < pages.get(currentPageIndex).getPositionCount(), "Already finished");
+            Type type = types.get(field);
+            return type.getDouble(pages.get(currentPageIndex).getBlock(field), position);
+        }
+
+        @Override
+        public Slice getSlice(int field)
+        {
+            checkState(position >= 0, "Not yet advanced");
+            checkState(position < pages.get(currentPageIndex).getPositionCount(), "Already finished");
+            Type type = types.get(field);
+            return type.getSlice(pages.get(currentPageIndex).getBlock(field), position);
+        }
+
+        @Override
+        public Object getObject(int field)
+        {
+            checkState(position >= 0, "Not yet advanced");
+            checkState(position < pages.get(currentPageIndex).getPositionCount(), "Already finished");
+            Type type = types.get(field);
+            return type.getObject(pages.get(currentPageIndex).getBlock(field), position);
+        }
+
+        @Override
+        public boolean isNull(int field)
+        {
+            checkState(position >= 0, "Not yet advanced");
+            checkState(position < pages.get(currentPageIndex).getPositionCount(), "Already finished");
+            return pages.get(currentPageIndex).getBlock(field).isNull(position);
+        }
+
+        @Override
+        public void close() {}
     }
 }
